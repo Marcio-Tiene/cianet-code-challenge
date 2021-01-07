afa617277f0b54fd170a5452f333a2d80b23e5c3
Marcio Tiene <marciorft@gmail.com>
2021-01-07 11:54:25 -0300

    clearing webvitals report

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,8 @@ import MapProvider from './components/MapProvider';
 import Header from './components/Header';
 import AddressFormModal from './components/AddressFormModal';
 import Footer from './components/Footer';
-import reportWebVitals from './reportWebVitals';
 
 function App() {
-  reportWebVitals(console.log);
   return (
     <Main>
       <Header />
